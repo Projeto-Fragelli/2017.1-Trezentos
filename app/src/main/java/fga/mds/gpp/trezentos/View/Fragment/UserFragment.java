3e1ee8eba3bb88d6c615cffd56bc372803f21e24
GeovanaRamos <geovannna.1998@gmail.com>
2018-07-19 15:13:00 -0300

    Adding alert to confirm account deletion

--- app/src/main/java/fga/mds/gpp/trezentos/View/Fragment/UserFragment.java
+++ app/src/main/java/fga/mds/gpp/trezentos/View/Fragment/UserFragment.java
@@ -1,6 +1,8 @@
 package fga.mds.gpp.trezentos.View.Fragment;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
@@ -74,7 +76,7 @@ public class UserFragment extends Fragment implements View.OnClickListener{
 
         exitButton = view.findViewById(R.id.exit_button);
         changePasswordButton = view.findViewById(R.id.button_change_password);
-        deleteAccountButton = view.findViewById(R.id.button_delete_account);
+        deleteAccountButton = view.findViewById(R.id.button_delete_user);
 
         exitButton.setOnClickListener(this);
         changePasswordButton.setOnClickListener(this);
@@ -108,12 +110,34 @@ public class UserFragment extends Fragment implements View.OnClickListener{
                 goChangePasswordScreen();
                 break;
             }
-            case R.id.button_delete_account: {
-                String response = userAccountControl.deleteUser(userId);
-                verifyResponse(response);
+            case R.id.button_delete_user: {
+                confirmUserDeletion();
                 break;
             }
         }
+    }
+
+    private void confirmUserDeletion() {
+
+        new AlertDialog.Builder(getContext())
+                .setTitle("Deletar usuário")
+                .setMessage("Tem certeza que deseja excluir sua conta de usuário?")
+                .setPositiveButton(
+                        "Sim",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                String response = userAccountControl.deleteUser(userId);
+                                verifyResponse(response);
+                            }
+                        }
+                )
+                .setNegativeButton(
+                        "Não",
+                        null
+                )
+                .show();
+
     }
 
     private void verifyResponse(String response){
