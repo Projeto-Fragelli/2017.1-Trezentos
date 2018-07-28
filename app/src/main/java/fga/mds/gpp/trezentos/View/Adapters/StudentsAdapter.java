4cb83d68d40908ccd04be1440963131f4185c298
GeovanaRamos <geovannna.1998@gmail.com>
2018-07-28 09:37:40 -0300

    Finalizing students fragment grade assignment

--- app/src/main/java/fga/mds/gpp/trezentos/View/Adapters/StudentsAdapter.java
+++ app/src/main/java/fga/mds/gpp/trezentos/View/Adapters/StudentsAdapter.java
@@ -1,13 +1,22 @@
 package fga.mds.gpp.trezentos.View.Adapters;
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.NumberPicker;
+
 import java.util.ArrayList;
+
+import fga.mds.gpp.trezentos.Model.Exam;
 import fga.mds.gpp.trezentos.Model.Student;
 import fga.mds.gpp.trezentos.Model.UserAccount;
 import fga.mds.gpp.trezentos.R;
@@ -20,6 +29,7 @@ import fga.mds.gpp.trezentos.View.ViewHolder.StudentsViewHolder;
 public class StudentsAdapter extends RecyclerView.Adapter implements View.OnClickListener {
     private ArrayList<Student> students;
     private Context context;
+    private StudentsViewHolder studentsViewHolder;
     private StudentsViewHolder.OnItemClickListener listener;
 
     public void setOnItemClickListener(StudentsViewHolder.OnItemClickListener listener) {
@@ -45,14 +55,21 @@ public class StudentsAdapter extends RecyclerView.Adapter implements View.OnClic
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
         Student student = students.get(position);
-        StudentsViewHolder studentsViewHolder = (StudentsViewHolder) holder;
-
         String name = student.getFisrtName() + " " + student.getLastName();
+
+        studentsViewHolder = (StudentsViewHolder) holder;
 
         studentsViewHolder.userAccountName.setText(name);
         studentsViewHolder.userAccountEmail.setText(student.getEmail());
-        studentsViewHolder.gradeTextView.setText(String.valueOf(student.getFirstGrade()));
+        studentsViewHolder.firstGradeTextView.setText(String.valueOf(student.getFirstGrade()));
         studentsViewHolder.secondGradeTextView.setText(String.valueOf(student.getSecondGrade()));
+
+        studentsViewHolder.firstGradeTextView.setOnClickListener(this);
+        studentsViewHolder.secondGradeTextView.setOnClickListener(this);
+
+        studentsViewHolder.firstGradeTextView.setTag(position);
+        studentsViewHolder.secondGradeTextView.setTag(position);
+
     }
 
     @Override
@@ -72,13 +89,75 @@ public class StudentsAdapter extends RecyclerView.Adapter implements View.OnClic
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.text_view_first_grade:
+                showGradePicker((Integer) v.getTag(), 1);
+                break;
+
+            case R.id.text_view_second_grade:
+                showGradePicker((Integer) v.getTag(), 2);
+                break;
+
+        }
 
     }
 
-//    public void setFilteredList (ArrayList<UserClass> filteredList){
-//        this.students = filteredList;
-//        notifyDataSetChanged();
-//    }
+    private void showGradePicker(final int position, final int gradeType){
+        Log.d("ALUNO INDEX", String.valueOf(position));
+
+        final Dialog d = new Dialog(context);
+        d.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        d.setContentView(R.layout.dialog);
+
+        final NumberPicker np1 = d.findViewById(R.id.numberPicker1);
+        final NumberPicker np2 = d.findViewById(R.id.numberPicker2);
+        Button buttonOK = d.findViewById(R.id.button_ok);
+        Button buttonCancel = d.findViewById(R.id.button_cancel);
+
+        np1.setMinValue(0);
+        np1.setMaxValue(9);
+        np1.setWrapSelectorWheel(false);
+
+        np2.setMinValue(0);
+        np2.setMaxValue(9);
+        np2.setWrapSelectorWheel(false);
+
+        buttonOK.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+
+                np1.clearFocus();
+                np2.clearFocus();
+
+                double np1Value = np1.getValue();
+                double np2Value = np2.getValue();
+                double value = np1Value + np2Value/10.0;
+
+                Student student = students.get(position);
+                if (gradeType == 1){
+                    student.setFirstGrade(value);
+                } else if (gradeType == 2 ){
+                    student.setSecondGrade(value);
+                }
+
+                notifyDataSetChanged();
+                Log.d("ESTUDANTE", student.getFisrtName());
+                d.dismiss();
+            }
+        });
+
+        buttonCancel.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                d.dismiss();
+            }
+        });
+
+        d.show();
+
+
+    }
+
+
+
 }
 
 
--- app/src/main/java/fga/mds/gpp/trezentos/View/ViewHolder/StudentsViewHolder.java
+++ app/src/main/java/fga/mds/gpp/trezentos/View/ViewHolder/StudentsViewHolder.java
@@ -1,63 +1,51 @@
 package fga.mds.gpp.trezentos.View.ViewHolder;
-
 import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.ContextThemeWrapper;
-import android.view.Gravity;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.NumberPicker;
-import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import fga.mds.gpp.trezentos.R;
-
-import static fga.mds.gpp.trezentos.R.id.student_email;
-import static fga.mds.gpp.trezentos.R.id.student_name;
 
 public class StudentsViewHolder  extends RecyclerView.ViewHolder implements View.OnClickListener {
 
     public TextView userAccountName;
     public TextView userAccountEmail;
-    public TextView gradeTextView;
+    public TextView firstGradeTextView;
     public TextView secondGradeTextView;
-    public LinearLayout gradeLayout;
-    public LinearLayout secondGradeLayout;
-    public ImageButton imageButtonMore;
-
-
-    public StudentsViewHolder(View view) {
-        super(view);
-
-        userAccountName = view.findViewById(student_name);
-        userAccountEmail = view.findViewById(student_email);
-        gradeLayout = view.findViewById(R.id.gradeLayout);
-        secondGradeLayout = view.findViewById(R.id.second_grade_layout);
-        gradeTextView = view.findViewById(R.id.text_view_grade);
-        secondGradeTextView = view.findViewById(R.id.text_view_second_grade);
-        imageButtonMore = view.findViewById(R.id.more_options);
-
-        view.setOnClickListener(this);
-    }
+    public ImageButton moreOptionsButton;
 
     // Define listener member variable
-    private StudentsViewHolder.OnItemClickListener listener;
+    private OnItemClickListener listener;
 
     // Define the listener interface
     public interface OnItemClickListener {
         void onItemClick(View itemView, int position);
     }
     // Define the method that allows the parent activity or fragment to define the listener
-    public void setOnItemClickListener(StudentsViewHolder.OnItemClickListener listener) {
+    public void setOnItemClickListener(OnItemClickListener listener) {
         this.listener = listener;
+    }
+
+
+    public StudentsViewHolder(View view) {
+        super(view);
+
+        userAccountName = view.findViewById(R.id.student_name);
+        userAccountEmail = view.findViewById(R.id.student_email);
+        firstGradeTextView = view.findViewById(R.id.text_view_first_grade);
+        secondGradeTextView = view.findViewById(R.id.text_view_second_grade);
+        moreOptionsButton = view.findViewById(R.id.more_options);
+
+        firstGradeTextView.setOnClickListener(this);
+
+
     }
 
     @Override
@@ -73,114 +61,5 @@ public class StudentsViewHolder  extends RecyclerView.ViewHolder implements View
     }
 
 
-//    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-//    public void showPopup(View v) {
-//
-//        ContextThemeWrapper contextThemeWrapper = new ContextThemeWrapper(getActivity(), R.style.PopupMenuOverlapAnchor);
-////            PopupMenu popupMenu = new PopupMenu(contextThemeWrapper, this);
-//
-//        PopupMenu popup = new PopupMenu(contextThemeWrapper, getView(), Gravity.RIGHT);
-//
-//        MenuInflater inflater = popup.getMenuInflater();
-//        inflater.inflate(R.menu.menu_student, popup.getMenu());
-//        popup.show();
-//    }
-//
-//    @Override
-//    public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-//
-//    }
-//
-//    int showGradePicker(final int CLICK) {
-//        final Dialog d = new Dialog(getContext());
-//        d.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-//        d.setContentView(R.layout.dialog);
-//
-//        final NumberPicker np1 = d.findViewById(R.id.numberPicker1);
-//        final NumberPicker np2 = d.findViewById(R.id.numberPicker2);
-//
-//        Button b1 = d.findViewById(R.id.button1);
-//        Button b2 = d.findViewById(R.id.button2);
-//        Button buttonOK300 = d.findViewById(R.id.button1);
-//
-//        np1.setMinValue(0);  // min value 0
-//        np1.setMaxValue(10); // max value 100
-//        np1.setWrapSelectorWheel(false);
-//        np1.setOnValueChangedListener(this);
-//
-//        np2.setMinValue(0); // min value 0
-//        np2.setMaxValue(99); // max value 100
-//        np2.setWrapSelectorWheel(false);
-//        np2.setOnValueChangedListener(this);
-//
-//        if(CLICK == 1) {
-//            b1.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    gradeTextView.setText(String.valueOf(np1.getValue()) + "." +
-//                            String.valueOf(String.format("%02d", np2.getValue()))); //set the value to textview
-//
-//                    Double grade = Double.valueOf(gradeTextView.getText().toString());
-//                    String email = userAccountName.getText().toString();
-//
-//                    mapEmailAndGrade.put(email, grade);
-//                    userExam.setFirstGrades(mapEmailAndGrade.toString());
-//
-//                    grade = Double.valueOf(gradeTextView.getText().toString());
-//                    email = userAccountName.getText().toString();
-//
-//                    Log.i("MAP", email);
-//                    Log.d("TAMANHOMAPA", Integer.toString(mapEmailAndGrade.size()));
-//
-//                    mapEmailAndGrade.put(email, grade);
-//
-//                    userExam.setFirstGrades(mapEmailAndGrade.toString());
-//                    userExam.setSecondGrades(mapEmailAndGrade.toString());
-//
-//                    Log.d("TAMANHOMAPA", userExam.getFirstGrades());
-//
-//                    d.dismiss();
-//                }
-//            });
-//        }else {
-//
-//            secondGradeTextView.setText(userExam.getSecondGrades());
-//
-//            buttonOK300.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    String EMAIL;
-//                    Double GRADE;
-//                    secondGradeTextView.setText(String.valueOf(np1.getValue()) + "." +
-//                            String.valueOf(String.format
-//                                    ("%02d", np2.getValue())));
-//
-//                    GRADE = Double.valueOf(secondGradeTextView.getText().toString());
-//
-//                    EMAIL = userAccountName.getText().toString();
-//
-//                    Log.d("TAMANHOMAPA", Integer.toString(mapEmailAndGrade.size()));
-//
-//                    mapEmailAndGrade.put(EMAIL, GRADE);
-//
-//                    userExam.setSecondGrades(mapEmailAndGrade.toString());
-//
-//                    Log.d("TAMANHOMAPA", userExam.getSecondGrades());
-//
-//                    d.dismiss();
-//                }
-//            });
-//        }
-//
-//        b2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                d.dismiss(); // dismiss the dialog
-//            }
-//        });
-//        d.show();
-//
-//        return CLICK;
-//    }
 
 }
